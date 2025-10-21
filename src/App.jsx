2213f355fe726ed8e319b20e7f1d3eead914d382
Ignacio Vetrano <ivetrano@fi.uba.ar>
2025-10-21 16:20:59 -0300

    Agrego paquetes Tailwind React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Button from 'react-bootstrap/Button';
 import './App.css'
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
   return (
     <>
       <div>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <Button variant="dark">Dark</Button>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
