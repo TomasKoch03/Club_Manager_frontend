f63771a68937d1162158482f902badc8b81b82bf
MateoRiat <mriat@fi.uba.ar>
2025-10-21 14:42:08 -0300

    feat(start_dev.sh): Agrego al script un condicional para que funcione tanto en versiones de docker-compose como docker compose

--- start_dev.sh
+++ start_dev.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-docker compose up --build
+
+if docker compose version >/dev/null 2>&1; then
+    docker compose up --build
+elif docker-compose --version >/dev/null 2>&1; then
+    docker-compose up --build
+else
+    echo "Error: No se encontró docker compose ni docker-compose"
+    exit 1
+fi
