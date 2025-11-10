08bd5f7f1a8736d79626e90a6704459a85886b52
Ignacio Vetrano <ivetrano@fi.uba.ar>
2025-11-10 10:53:22 -0300

    feat: Ahora la URL de la API es una variable de entorno

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8001';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiRequest = async (endpoint, options = {}) => {
     const token = localStorage.getItem('accessToken');
