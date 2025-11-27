c6c08eb1d507c7b2e1354ed34dbbb04481da7af2
Ignacio Vetrano <ivetrano@fi.uba.ar>
2025-11-27 12:29:35 -0300

    fix: gh pages 3

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Club_Manager_frontend/">
+    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || '/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
